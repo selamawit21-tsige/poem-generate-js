4909eca266b2045e8d6dfa8f31e507053c21ea5d
selamawit haileselasse <139425406+selamawit21-tsige@users.noreply.github.com>
2025-02-16 15:58:13 -0800

    save-all

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ function generatePoem(event) {
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a spanish poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
